8601d55f1bb82ff3611c1b3f8caa17bbe8b474d2
Vijay Raja <vijayraja0751@gmail.com>
2024-11-18 00:17:09 +0530

    change cidr

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["127.0.0.1/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
