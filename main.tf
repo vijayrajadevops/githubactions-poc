b8224eb7008a963f5e98f7e0aa7a2511fd0e7296
Vijay Raja <vijayraja0751@gmail.com>
2024-11-19 13:44:25 +0530

    fix code

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "ModMed" {
 
 
 resource "aws_subnet" "ModMed2" {
-  vpc_id                  = aws_vpc.ModMed2.id
+  vpc_id                  = aws_vpc.ModMed.id
   cidr_block              = var.subnet_cidr_block
   availability_zone       = "us-east-1b"  # Modify based on your region
   map_public_ip_on_launch = true
