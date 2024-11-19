b9de8d3613abd6e1b99a1007387737c246b85f76
Vijay Raja <vijayraja0751@gmail.com>
2024-11-19 13:50:09 +0530

    update code

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "vpc_cidr_block" {
 variable "subnet_cidr_block" {
   description = "The CIDR block for the subnet."
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "10.0.2.0/24"
 }
 
 variable "instance_type" {
