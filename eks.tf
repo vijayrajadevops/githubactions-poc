c465cc91845b78a9d2a2828e237fb9762f188565
Vijay Raja <vijayraja0751@gmail.com>
2024-11-19 13:40:53 +0530

    add eks code

---
+++ eks.tf
@@ -0,0 +1,55 @@
+resource "aws_eks_cluster" "ModMed" {
+  name     = "EKS-cluster-ModMed"
+  role_arn = aws_iam_role.ModMed.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.ModMed.id, aws_subnet.ModMed2.id]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.ModMed-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.ModMed-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.ModMed.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.ModMed.certificate_authority[0].data
+}
+
+
+#IAM ROLE code 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ModMed" {
+  name               = "eks-cluster-ModMed"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ModMed-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.ModMed.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "ModMed-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.ModMed.name
+}
--- main.tf
+++ main.tf
@@ -21,6 +21,18 @@ resource "aws_subnet" "ModMed" {
   }
 }
 
+
+resource "aws_subnet" "ModMed2" {
+  vpc_id                  = aws_vpc.ModMed2.id
+  cidr_block              = var.subnet_cidr_block
+  availability_zone       = "us-east-1b"  # Modify based on your region
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "ModMed-subnet"
+  }
+}
+
+
 # Internet Gateway 
 resource "aws_internet_gateway" "ModMed" {
   vpc_id = aws_vpc.ModMed.id
